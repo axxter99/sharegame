3e5acd2d7593b6982fb20bf3d0f865ce23ea985a
David Horwitz <dhorwitz@gmail.com>
2015-08-17 17:21:58 +0200

    NOJIRA

--- tool/src/java/org/sakaiproject/sharegame/tool/producers/Trade.java
+++ tool/src/java/org/sakaiproject/sharegame/tool/producers/Trade.java
@@ -2,23 +2,23 @@ package org.sakaiproject.sharegame.tool.producers;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.sharegame.model.trade;
 
 import uk.org.ponder.rsf.components.UIContainer;
 import uk.org.ponder.rsf.view.ComponentChecker;
-import uk.org.ponder.rsf.view.DefaultView;
 import uk.org.ponder.rsf.view.ViewComponentProducer;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 
-public class ShareGame implements DefaultView, ViewComponentProducer {
+public class Trade implements ViewComponentProducer {
+
+	private static Log log = LogFactory.getLog(trade.class);
 	
-	private static Log log = LogFactory.getLog(ShareGame.class);
-	
-	public static final String VIEW_ID = "share";
+	public static final String VIEW_ID = "trade";
 	public String getViewID() {
 		return VIEW_ID;
 	}
 	
 	public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
-		log.info("ShareGame!");
+		log.info("ShareGame!: trade!");
 	}
 }
