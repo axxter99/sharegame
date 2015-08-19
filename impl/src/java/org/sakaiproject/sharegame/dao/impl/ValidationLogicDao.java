e19db6074dc8f523885773fdffd5dc104d602079
David Horwitz <dhorwitz@gmail.com>
2015-08-19 13:08:41 +0200

    NOJIRA

---
+++ impl/src/java/org/sakaiproject/sharegame/dao/impl/ValidationLogicDao.java
@@ -0,0 +1,10 @@
+package org.sakaiproject.sharegame.dao.impl;
+
+import org.sakaiproject.sharegame.dao.ValidationDao;
+import org.sakaiproject.genericdao.hibernate.HibernateCompleteGenericDao;
+
+public class ValidationLogicDao extends HibernateCompleteGenericDao implements ValidationDao  {
+
+
+
+}
